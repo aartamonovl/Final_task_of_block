b2a57b455ab6f1045135dc24451eb06a6afdec9b
a.artamonov <a.artamonov@gb.ru>
2022-12-04 11:48:54 +0300

    Task 4 is done

--- Program.cs
+++ Program.cs
@@ -8,17 +8,50 @@
     Console.Write($"\b\b]\n");
 }
 
-Console.WriteLine("Введите желаемое количество элементов в массиве: ");
-int size = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine($"Введите {size} элементов после каждого нажимая кнопку Enter. ");
-Console.WriteLine($"Пустое значение не считается за элемент массива.");
-string [] array = new string [size];
-for (int i = 0; i < size; i++)
+string [] FindElemLenLessThree (string [] array)
 {
-    string elem = Console.ReadLine();
-    if (elem != string.Empty)
-        array[i] = elem;
-    else i--;
+    int changedSize = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+            changedSize++;
+    }
+    if (changedSize != 0)
+    {
+        string [] changedArray = new string [changedSize];
+        int j = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= 3)
+                changedArray[j++] = array[i];
+        }
+        return changedArray;
+    }
+    else 
+    {
+        string [] zeroArray = new string [1];
+        zeroArray[0] = string.Empty;
+        return zeroArray;
+    }
 }
 
-ShowArray(array);
+Console.WriteLine("Введите желаемое количество элементов в массиве: ");
+int size = Convert.ToInt32(Console.ReadLine());
+if (size > 0)
+{
+    Console.WriteLine($"Введите элементы после каждого нажимая кнопку Enter. ");
+    Console.WriteLine($"Пустое значение не считается за элемент массива.");
+    string [] array = new string [size];
+    for (int i = 0; i < size; i++)
+    {
+        string elem = Console.ReadLine();
+        if (elem != string.Empty)
+            array[i] = elem;
+        else i--;
+    }
+
+    ShowArray(array);
+    ShowArray(FindElemLenLessThree(array));
+}
+else
+    Console.WriteLine("Размер массива должен быть больше 0. Попробуйте снова.");
